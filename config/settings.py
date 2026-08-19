e5490da0a18f664c38aa1be8709814a952a3e182
Ana Gabriela Ribeiro <anagabrielaribeiro.aruja@gmail.com>
2026-08-19 18:41:39 -0300

    chore: configura secrect key

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+fh1tzpk7_rrn*agj6#7eghns7(&1c8$c&b&&_w7=##ilr4i15"
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-dev-key'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
