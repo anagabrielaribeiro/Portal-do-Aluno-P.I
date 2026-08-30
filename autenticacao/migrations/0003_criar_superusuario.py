dc41aa1f17c47d86f90fbcc88444e2dc84a72896
vitoria souza <vitoria.souza.142024@gamil.com>
2026-08-30 16:26:27 -0300

    Adiciona documentação técnica, migration de usuário de teste e ajustes visuais

---
+++ autenticacao/migrations/0003_criar_superusuario.py
@@ -0,0 +1,31 @@
+from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+# essa função só será executada quando rodar o migration
+def criar_superusuario(apps, schema_editor):
+    Usuario = apps.get_model('autenticacao', 'Usuario')
+
+    #Cria o registro do superusuariono banco, com o nome e senha criptografada
+    Usuario.objects.create(
+        username='Professor.Teste',
+        password=make_password('PortalPI@2026'),
+        is_staff=True, # acesso ao painel administrativo
+        is_superuser=True, # recebe todas as permissões do sistema
+        is_active=True, # deixa a conta ativa
+    )
+
+# função só será executada caso a migration seja revertida
+def remover_superusuario(apps, schema_editor):
+    Usuario = apps.get_model('autenticacao', 'Usuario')
+    Usuario.objects.filter(username='professor').delete() # remove o usuario criado caso a migration seja desfeita 
+
+# classe que define a migration 
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('autenticacao', '0002_usuario_chave_2fa'), # sera executada depois da 0002
+    ]
+
+    operations = [
+        migrations.RunPython(criar_superusuario, remover_superusuario), # executa o que está na primeira função ao aplicar, e a segunda função ao reverter a migration
+    ]
