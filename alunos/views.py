e9e1a61d110b3579bf6be0f8d1344f5fcd9c1a68
vitoria souza <vitoria.souza.142024@gamil.com>
2026-09-09 22:36:04 -0300

    Ajusta  erro no form para edição do endereço e telefone e adiciona seta de voltar ao dashboard na página de dados pessoais

--- alunos/views.py
+++ alunos/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import Aluno
+from .forms import AlunoContatoForm
 
 # se não estiver logado redireciona para a tela Login
 @login_required
@@ -20,12 +21,22 @@ def dados_pessoais(request):
 
     # busca a matricula ativa do aluno
     matricula_atual = aluno.matriculas.filter(status='ativa').select_related('curso', 'turma').first()
-   
+
+    # trata o envio do formulario de telefone e endereco, a página ve pra ca quando o aluo clica em salvar
+    if request.method == 'POST': 
+        form = AlunoContatoForm(request.POST, instance=aluno) # request.Post tem os dados digitados e o instance=aluno não deixa criar um aluno novo e sim atualizar
+        if form.is_valid(): # is_valid() roda as validaões do form/model como campo origatório
+            form.save() # salva no bd
+            messages.success(request, 'Dados de contato atualizados com sucesso.')
+            return redirect('dados_pessoais')# redireciona de volta pra mesma pagina 
+    else:
+        form = AlunoContatoForm(instance=aluno)
 
     # dados que serão enviado para o html
     context = {
         'aluno': aluno,
         'matricula_atual': matricula_atual,
+        'form': form,
     }
     # renderiza a pagina 
     return render(request, 'alunos/dados_pessoais.html', context)
