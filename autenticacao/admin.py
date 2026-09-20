dfca4af2490905b6f5443c5d33313bbf36522b1b
vitoria souza <vitoria.souza.142024@gamil.com>
2026-09-20 15:35:25 -0300

    Cria login exclusivo para colaborador da instituição, reaproveitando 2FA e recuperação de senha do fluxo do aluno

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 from .models import Usuario, LogAutenticacao
-
+from .models import Colaborador
 
 admin.site.register(Usuario, UserAdmin)
+
 
 @admin.register(LogAutenticacao)
 class LogAutenticacaoAdmin(admin.ModelAdmin):
     list_display = ('data_hora', 'usuario', 'evento', 'ip')  # colunas mostradas na listagem do admin
     readonly_fields = [f.name for f in LogAutenticacao._meta.fields]  # todos os campos ficam somente leitura
 
-    
+@admin.register(Colaborador)
+class ColaboradorAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'cargo')  # colunas mostradas na listagem do admin
